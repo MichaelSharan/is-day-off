35cbd1866d11c432f70dcfbf6c70bcc38a4c6a21
Michael Sharan <you@example.com>
2026-03-10 18:23:52 +0400

    feat: made day off status showing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeRu from '@angular/common/locales/ru';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+
+registerLocaleData(localeRu);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'ru' }
   ]
 };
--- src/app/features/today-status/pages/today-status-main/today-status-main.component.ts
+++ src/app/features/today-status/pages/today-status-main/today-status-main.component.ts
@@ -1,12 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { DateStatusService } from '../../../../core/api/date-status.service';
 
 @Component({
   selector: 'app-today-status-main',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './today-status-main.component.html',
   styleUrl: './today-status-main.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TodayStatusMainComponent {
-
+  today = new Date();
+  private dateStatusService = inject(DateStatusService);
+  dateStatus = toSignal(this.dateStatusService.getDateStatusString(this.today, 'ru'), { initialValue: 'Загрузка...' });
 }
