2afcb801b793245b328b639cc2ae421b982e7ce6
Michael Sharan <you@example.com>
2026-03-10 18:25:32 +0400

    feat: added today status into routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./features/today-status/pages/today-status-main/today-status-main.component').then(m => m.TodayStatusMainComponent)
+    }
+];
